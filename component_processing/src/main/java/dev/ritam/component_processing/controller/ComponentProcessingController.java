3c7a7a6769c83176680b8f5def9ecf77b5833f3e
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-04-19 17:38:35 +0530

    Add validation for component-processing microservice

--- component_processing/src/main/java/dev/ritam/component_processing/controller/ComponentProcessingController.java
+++ component_processing/src/main/java/dev/ritam/component_processing/controller/ComponentProcessingController.java
@@ -4,19 +4,27 @@ import dev.ritam.component_processing.model.ProcessRequest;
 import dev.ritam.component_processing.service.ComponentProcessingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/component-processing/api")
 @RequiredArgsConstructor
+@Validated
 public class ComponentProcessingController {
     private final ComponentProcessingService componentProcessingService;
 
     @GetMapping("/process-detail")
-    public ResponseEntity<?> processDetail(@RequestBody ProcessRequest processRequest) {
+    public ResponseEntity<?> processDetail(
+            @RequestBody
+            @Valid
+                    ProcessRequest processRequest
+    ) {
         return ResponseEntity.ok(componentProcessingService.processDetail(processRequest));
     }
 }
--- component_processing/src/main/java/dev/ritam/component_processing/model/ProcessRequest.java
+++ component_processing/src/main/java/dev/ritam/component_processing/model/ProcessRequest.java
@@ -5,14 +5,24 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class ProcessRequest {
+    @NotBlank(message = "Name can not be blank")
     private String name;
+    @NotBlank(message = "Contact number can not be blank")
+    @Pattern(regexp = "^\\d{10}$", message = "Contact number must be 10 characters long and should consist of digits")
     private String contactNumber;
+    @NotBlank(message = "Component type can not be blank")
     private String componentType;
+    @NotBlank(message = "Component name can not be blank")
     private String componentName;
+    @Min(value = 1, message = "Quantity must be more than 0")
     private int quantity;
 }
