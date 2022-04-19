3be38eca7ef953df2d660f87404e43f4cdea852e
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-04-19 17:12:33 +0530

    Add validation for packaging-and-delivery microservice

--- packaging_and_delivery/src/main/java/dev/ritam/packaging_and_delivery/controller/PackagingAndDeliveryController.java
+++ packaging_and_delivery/src/main/java/dev/ritam/packaging_and_delivery/controller/PackagingAndDeliveryController.java
@@ -3,19 +3,28 @@ package dev.ritam.packaging_and_delivery.controller;
 import dev.ritam.packaging_and_delivery.service.PackagingAndDeliveryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @RestController
 @RequestMapping("/packaging-and-delivery/api")
 @RequiredArgsConstructor
+@Validated
 public class PackagingAndDeliveryController {
     private final PackagingAndDeliveryService packagingAndDeliveryService;
 
     @GetMapping("/packaging-and-delivery-charge")
     public @ResponseBody
     ResponseEntity<?> getPackagingAndDeliveryCharge(
-            @RequestParam String componentType,
-            @RequestParam Integer count
+            @RequestParam
+            @NotBlank(message = "Component Type can not be empty")
+                    String componentType,
+            @RequestParam
+            @Min(value = 0, message = "Count must be more than 0")
+                    Integer count
     ) {
         return ResponseEntity.ok(packagingAndDeliveryService
                 .getPackagingAndDeliveryCharge(componentType, count));
