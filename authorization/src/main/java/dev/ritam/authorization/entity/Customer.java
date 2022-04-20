d050dcdd2af43b806d74d406e780e5ad97f052c8
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-04-20 16:10:12 +0530

    Create Customer entity

---
+++ authorization/src/main/java/dev/ritam/authorization/entity/Customer.java
@@ -0,0 +1,56 @@
+package dev.ritam.authorization.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Collection;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class Customer implements UserDetails {
+    @Id
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String contactNumber;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
