8474706f952e2353343ea63d4d701ea9f2ad6d2c
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-05-17 19:05:24 +0530

    Write tests for SwaggerControllers

---
+++ packaging_and_delivery/src/test/java/dev/ritam/packaging_and_delivery/controller/SwaggerControllerTest.java
@@ -0,0 +1,26 @@
+package dev.ritam.packaging_and_delivery.controller;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class SwaggerControllerTest {
+    @InjectMocks
+    SwaggerController underTest;
+
+    @Test
+    void docs() {
+        // Given
+        String expected = "redirect:/swagger-ui/";
+
+        // When
+        String actual = underTest.docs();
+
+        // Then
+        MatcherAssert.assertThat(actual, Matchers.is(expected));
+    }
+}
