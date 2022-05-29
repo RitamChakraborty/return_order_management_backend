8ec815d46d2ac3dffbd03e0955ab171963f2f23b
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-05-29 23:03:23 +0530

    Add Open Api and actuator dependency

--- component_processing/src/main/java/dev/ritam/component_processing/ComponentProcessingApplication.java
+++ component_processing/src/main/java/dev/ritam/component_processing/ComponentProcessingApplication.java
@@ -1,10 +1,12 @@
 package dev.ritam.component_processing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@OpenAPIDefinition
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
--- packaging_and_delivery/src/main/java/dev/ritam/packaging_and_delivery/PackagingAndDeliveryApplication.java
+++ packaging_and_delivery/src/main/java/dev/ritam/packaging_and_delivery/PackagingAndDeliveryApplication.java
@@ -1,9 +1,11 @@
 package dev.ritam.packaging_and_delivery;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@OpenAPIDefinition
 @SpringBootApplication
 @EnableEurekaClient
 public class PackagingAndDeliveryApplication {
