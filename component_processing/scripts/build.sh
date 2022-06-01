17af7e3c8f56398e76ebee259caaafddbcbb411a
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-06-01 18:59:44 +0530

    Deploy component_processing

--- component_processing/scripts/build.sh
+++ component_processing/scripts/build.sh
@@ -5,5 +5,22 @@ cd ..
 
 echo 'Creating JAR ...'
 mvn clean install
+echo 'Removing old Docker container ...'
+docker container rm -f component-processing
+echo 'Removing old Docker image ...'
+docker rmi ritamchakraborty/return_order_component_processing:1.0
+docker rmi component_processing:1.0
 echo 'Creating docker image ...'
 mvn spring-boot:build-image
+docker tag component_processing:1.0 ritamchakraborty/return_order_component_processing:1.0
+echo 'Pushing image to Docker Hub ...'
+docker push ritamchakraborty/return_order_component_processing:1.0
+echo 'Running docker image ...'
+docker run -it --name component-processing \
+  --network return-order-network \
+  -e CONFIG_SERVER_URL=http://config-server:8286 \
+  -e DB_USERNAME=ritam \
+  -e DB_PASSWORD=password \
+  -e PACKAGING_AND_DELIVERY_API_URL=http://packaging-and-delivery:8082 \
+  -p 8081:8081 \
+  ritamchakraborty/return_order_component_processing:1.0
