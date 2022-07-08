9f60fda4a04abe1362d9f3c3908150df3ef9b43d
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-07-09 03:35:14 +0530

    Deploy packaging-and-delivery

--- packaging_and_delivery/Heroku.Dockerfile
+++ packaging_and_delivery/Heroku.Dockerfile
@@ -1,2 +1,3 @@
-FROM ritamchakraborty/return_order_packaging_and_delivery:1.0
-ARG CONFIG_SERVER_URL=https://config-server-ritam.herokuapp.com
+FROM openjdk:11
+ARG JAR_FILE=target/*.jar
+COPY $JAR_FILE app.jar
--- packaging_and_delivery/src/main/java/dev/ritam/packaging_and_delivery/PackagingAndDeliveryApplication.java
+++ packaging_and_delivery/src/main/java/dev/ritam/packaging_and_delivery/PackagingAndDeliveryApplication.java
@@ -3,11 +3,9 @@ package dev.ritam.packaging_and_delivery;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @OpenAPIDefinition
 @SpringBootApplication
-@EnableEurekaClient
 public class PackagingAndDeliveryApplication {
     public static void main(String[] args) {
         SpringApplication.run(PackagingAndDeliveryApplication.class, args);
