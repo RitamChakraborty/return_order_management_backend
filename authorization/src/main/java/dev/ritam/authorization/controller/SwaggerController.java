a936169d61683cda5199fe868e31ff3bb484e11c
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-07-09 01:30:06 +0530

    Update swagger controller to '/' endpoint in authorization

--- authorization/src/main/java/dev/ritam/authorization/controller/SwaggerController.java
+++ authorization/src/main/java/dev/ritam/authorization/controller/SwaggerController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/authorization/api")
+@RequestMapping("/")
 public class SwaggerController {
     @GetMapping
     public String docs() {
