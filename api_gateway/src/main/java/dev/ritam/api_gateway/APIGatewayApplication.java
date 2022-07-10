5bf1cb79f34fbdbb1d25936817f90720152a8920
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-07-10 08:26:24 +0530

    Deploy api-gateway microservice

--- api_gateway/src/main/java/dev/ritam/api_gateway/APIGatewayApplication.java
+++ api_gateway/src/main/java/dev/ritam/api_gateway/APIGatewayApplication.java
@@ -2,12 +2,8 @@ package dev.ritam.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableHystrix
 public class APIGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(APIGatewayApplication.class, args);
--- api_gateway/src/main/java/dev/ritam/api_gateway/filter/AuthenticationFilter.java
+++ api_gateway/src/main/java/dev/ritam/api_gateway/filter/AuthenticationFilter.java
@@ -3,48 +3,65 @@ package dev.ritam.api_gateway.filter;
 import dev.ritam.api_gateway.exception.AuthenticationTokenNotFoundException;
 import dev.ritam.api_gateway.exception.BadTokenException;
 import dev.ritam.api_gateway.model.CustomerResponse;
-import lombok.RequiredArgsConstructor;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.util.Objects;
 
 @Component
-@RequiredArgsConstructor
-public class AuthenticationFilter implements GatewayFilter {
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
     private static final String CUSTOMER_EMAIL_HEADER = "x-auth-customer-email";
-    private final WebClient.Builder webClientBuilder;
+    private WebClient.Builder webClientBuilder;
+
+    public AuthenticationFilter() {
+        super(Config.class);
+    }
+
+    @Autowired
+    public void setWebClientBuilder(WebClient.Builder webClientBuilder) {
+        this.webClientBuilder = webClientBuilder;
+    }
 
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-            throw new AuthenticationTokenNotFoundException("Authorization token not found");
-        }
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                throw new AuthenticationTokenNotFoundException("Authorization token not found");
+            }
 
-        String authorizationHeader =
-                Objects.requireNonNull(exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION)).get(0);
+            String authorizationHeader =
+                    Objects.requireNonNull(exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION)).get(0);
+            String authenticationUrl = config.getAuthenticationUrl();
 
-        return webClientBuilder
-                .build()
-                .get()
-                .uri("lb://authorization/authorization/api/authenticate")
-                .header(HttpHeaders.AUTHORIZATION, authorizationHeader)
-                .retrieve()
-                .onStatus(HttpStatus::isError, response -> Mono.error(new BadTokenException("Bad token")))
-                .bodyToMono(CustomerResponse.class)
-                .map(customerResponse -> {
-                    exchange
-                            .getRequest()
-                            .mutate()
-                                    .header(CUSTOMER_EMAIL_HEADER, customerResponse.getEmail());
-                            return exchange;
-                        }
-                ).flatMap(chain::filter);
+            return webClientBuilder
+                    .build()
+                    .get()
+                    .uri(authenticationUrl + "/authorization/api/authenticate")
+                    .header(HttpHeaders.AUTHORIZATION, authorizationHeader)
+                    .retrieve()
+                    .onStatus(HttpStatus::isError, response -> Mono.error(new BadTokenException("Bad token")))
+                    .bodyToMono(CustomerResponse.class)
+                    .map(customerResponse -> {
+                                exchange
+                                        .getRequest()
+                                        .mutate()
+                                        .header(CUSTOMER_EMAIL_HEADER, customerResponse.getEmail());
+                                return exchange;
+                            }
+                    ).flatMap(chain::filter);
+        };
     }
+
+    @Data
+    public static class Config {
+        private String authenticationUrl;
+    }
+
 }
